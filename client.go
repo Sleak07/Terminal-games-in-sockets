1badad0660e9fc70647e17f6b22d0a1e9653a24a
Bimal Mallick <ghostridergothan8460@gmail.com>
2024-12-11 19:45:04 +0530

    Server side done, Client for tommorow

---
+++ client.go
@@ -0,0 +1,34 @@
+// client side
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	done      chan interface{}
+	interrupt chan os.Signal
+)
+
+func recieveHandler(connection *websocket.Conn) {
+	defer close(done)
+	for {
+		_, msg, err := connection.ReadMessage()
+		if err != nil {
+			log.Println("Error in recieve:", err)
+			return
+		}
+		log.Printf("Recieved: %s\n", msg)
+	}
+}
+func main() {
+	done = make(chan interface{})    // channel to indicate reciever handle is done
+	interrupt = make(chan os.Signal) // channel to listen for interrupt signal
+	signal.Notify(interrupt, os.Interrupt)
+
+}
